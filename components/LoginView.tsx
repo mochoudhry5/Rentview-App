e13bbdbe7d24434e85ef4ad60201eb5cce46918a
Momin Choudhry <mochoudhry5@gmail.com>
2023-09-03 12:06:48 -0500

    comment revision

--- components/LoginView.tsx
+++ components/LoginView.tsx
@@ -1,4 +1,3 @@
-// src/components/Login.tsx
 import React, { useState } from 'react';
 import { View, Text, TextInput, Button, StyleSheet, TouchableOpacity } from 'react-native';
 import { StackNavigationProp } from '@react-navigation/stack';
