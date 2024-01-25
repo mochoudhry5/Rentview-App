1f0eeda19528ff43252285c2b21c3d9e15391be8
mochoudhry5 <mochoudhry5@gmail.com>
2024-01-24 20:46:21 -0600

    Remove admin view

--- src/screens/ChatViews/ChatsScreen.tsx
+++ src/screens/ChatViews/ChatsScreen.tsx
@@ -1,6 +1,10 @@
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import React, {useEffect} from 'react';
-import {Channel as ChannelType} from 'stream-chat';
+import {
+  ChannelFilters,
+  Channel as ChannelType,
+  DefaultGenerics,
+} from 'stream-chat';
 import {ChannelList} from 'stream-chat-react-native';
 import {ChatStackParamList} from '../../utils/types';
 import {useChatContext} from '../../context/ChatContext';
@@ -14,6 +18,21 @@ type ChatsScreenProps = NativeStackScreenProps<
 const ChatsScreen: React.FC<ChatsScreenProps> = ({navigation}) => {
   const {currentChannel, setCurrentChannel} = useChatContext();
   const userId = auth.currentUser ? auth.currentUser.uid : '';
+  const adminId = '0bXFuQZ3OmRA09Tr311JSBizUjs2';
+  const filters: ChannelFilters<DefaultGenerics> = {
+    $and: [
+      {
+        members: {
+          $in: [userId],
+        },
+      },
+      {
+        members: {
+          $nin: [adminId],
+        },
+      },
+    ],
+  };
 
   const onSelect = (channel: ChannelType) => {
     setCurrentChannel(channel);
@@ -21,9 +40,7 @@ const ChatsScreen: React.FC<ChatsScreenProps> = ({navigation}) => {
     navigation.navigate('ChatRoom');
   };
 
-  return (
-    <ChannelList filters={{members: {$in: [userId]}}} onSelect={onSelect} />
-  );
+  return <ChannelList filters={filters} onSelect={onSelect} />;
 };
 
 export default ChatsScreen;
